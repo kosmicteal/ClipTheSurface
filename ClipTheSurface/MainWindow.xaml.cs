3300a9e023d489a4d2c2fbc54b5f9351a5b289c8
kosmicteal <69432458+kosmicteal@users.noreply.github.com>
2022-12-21 17:35:54 +0100

    Agregar archivos de proyecto.

---
+++ ClipTheSurface/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using GregsStack.InputSimulatorStandard.Native;
+using System;
+using System.Windows;
+
+namespace ClipTheSurface
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Boolean hidden = false;
+
+        public MainWindow()
+        {
+            SystemInteraction si = new SystemInteraction();
+            si.SPBToogle();
+
+            InitializeComponent();
+
+            Microsoft.Win32.SystemEvents.DisplaySettingsChanged += SystemEvents_DisplaySettingsChanged;
+            ReloadPosition();
+        }
+
+        void SystemEvents_DisplaySettingsChanged(object sender, EventArgs e)
+        {
+            ReloadPosition();
+        }
+
+        void ReloadPosition()
+        {
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+
+        private void shiftToogle_Click(object sender, RoutedEventArgs e)
+        {
+            SystemInteraction si = new SystemInteraction();
+            si.genericToogle_Click(VirtualKeyCode.LSHIFT, shiftToogle);
+        }
+
+        private void ctrlToogle_Click(object sender, RoutedEventArgs e)
+        {
+            SystemInteraction si = new SystemInteraction();
+            si.genericToogle_Click(VirtualKeyCode.LCONTROL, ctrlToogle);
+        }
+
+        private void altToogle_Click(object sender, RoutedEventArgs e)
+        {
+            SystemInteraction si = new SystemInteraction();
+            si.genericToogle_Click(VirtualKeyCode.LMENU, altToogle);
+        }
+
+        private void closeApp_Click(object sender, RoutedEventArgs e)
+        {
+            //failsafe to reset all modified key presses
+
+            SystemInteraction si = new SystemInteraction();
+            si.cleanKeyboard();
+            this.Close();
+        }
+
+        private void hideApp_Click(object sender, RoutedEventArgs e)
+        {
+            //failsafe to reset all modified key presses
+            if (!hidden) {
+            hideApp.HorizontalAlignment = HorizontalAlignment.Left;
+                hideApp.Content = "";
+
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width + 140;
+                hidden = true;
+
+            }
+            else
+            {
+
+                
+                var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+                this.Left = desktopWorkingArea.Right - this.Width;
+
+                hideApp.HorizontalAlignment = HorizontalAlignment.Right;
+                hideApp.Content = "";
+
+                hidden = false;
+            }
+        }
+
+
+    }
+}
---
+++ ClipTheSurface/SystemInteraction.cs
@@ -0,0 +1,57 @@
+﻿using GregsStack.InputSimulatorStandard;
+using GregsStack.InputSimulatorStandard.Native;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ClipTheSurface
+{
+    class SystemInteraction
+    {
+        public void genericToogle_Click(VirtualKeyCode key, Button keyButton)
+        {
+            var simulator = new InputSimulator();
+            var isKeyDown = simulator.InputDeviceState.IsKeyDown(key);
+
+            if (isKeyDown)
+            {
+                simulator.Keyboard.KeyUp(key);
+                keyButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#7F333333");
+            }
+            else
+            {
+                simulator.Keyboard.KeyDown(key);
+                keyButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#BF536280");
+            }
+        }
+
+        public void SPBToogle()
+        {
+            //This function kills all processes with this name if it's already running.
+            //To be used as a "Toogle" with Surface Pen's button.
+
+            cleanKeyboard(); //failsafe in case of unexpected termination
+
+            var current = Process.GetCurrentProcess();
+            var processlist = Process.GetProcessesByName(current.ProcessName);
+            if (processlist.Length > 1)
+            {
+                processlist.Where(t => t.Id != current.Id).ToList().ForEach(t => t.Kill());
+                current.Kill();
+            }
+        }
+
+        public void cleanKeyboard()
+        {
+            //This function cleans any modifier key status.
+            var simulator = new InputSimulator();
+            VirtualKeyCode[] keys = { VirtualKeyCode.SHIFT, VirtualKeyCode.LCONTROL, VirtualKeyCode.LMENU };
+            foreach (VirtualKeyCode vky in keys)
+            {
+                simulator.Keyboard.KeyUp(vky);
+            }
+        }
+    }
+}
