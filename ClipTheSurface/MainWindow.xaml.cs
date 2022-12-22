93b8141d30959ac2723a2f34bc4b02b4fe8eb6dd
kosmicteal <69432458+kosmicteal@users.noreply.github.com>
2022-12-22 12:37:32 +0100

    Added arrow keys and "Full Focus Mode" integrated with Clip Studio

--- ClipTheSurface/MainWindow.xaml.cs
+++ ClipTheSurface/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using GregsStack.InputSimulatorStandard.Native;
 using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace ClipTheSurface
@@ -10,6 +11,7 @@ namespace ClipTheSurface
     public partial class MainWindow : Window
     {
         private Boolean hidden = false;
+        public Boolean ffmode = false;
 
         public MainWindow()
         {
@@ -31,7 +33,7 @@ namespace ClipTheSurface
         {
             var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
             this.Left = desktopWorkingArea.Right - this.Width;
-            this.Top = desktopWorkingArea.Bottom - this.Height;
+            this.Top = (desktopWorkingArea.Bottom - this.Height) / 2;
         }
 
         private void shiftToogle_Click(object sender, RoutedEventArgs e)
@@ -64,29 +66,79 @@ namespace ClipTheSurface
         private void hideApp_Click(object sender, RoutedEventArgs e)
         {
             //failsafe to reset all modified key presses
-            if (!hidden) {
-            hideApp.HorizontalAlignment = HorizontalAlignment.Left;
+            if (!hidden)
+            {
+                //hideApp.HorizontalAlignment = HorizontalAlignment.Left;
+                UIButtons.Visibility = Visibility.Hidden;
                 hideApp.Content = "";
 
-            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
-            this.Left = desktopWorkingArea.Right - this.Width + 140;
+                //var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+                //this.Left = desktopWorkingArea.Right - this.Width + 140;
                 hidden = true;
 
             }
             else
             {
 
-                
-                var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
-                this.Left = desktopWorkingArea.Right - this.Width;
 
-                hideApp.HorizontalAlignment = HorizontalAlignment.Right;
+                //var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+                //this.Left = desktopWorkingArea.Right - this.Width;
+
+                //hideApp.HorizontalAlignment = HorizontalAlignment.Right;
+                UIButtons.Visibility = Visibility.Visible;
                 hideApp.Content = "";
 
                 hidden = false;
             }
         }
 
+        private async void tabletToogle_Click(object sender, RoutedEventArgs e)
+        {
+            var TTOriginal = tabletToogle.Content;
+            tabletToogle.Content = "\uF16A";
 
+            SystemInteraction si = new SystemInteraction();
+            ffmode = si.fullFocusMode(ffmode, tabletToogle);
+            await Task.Delay(3000);
+
+            tabletToogle.Content = TTOriginal;
+        }
+
+        private void upPrecision_Click(object sender, RoutedEventArgs e)
+        {
+            SystemInteraction si = new SystemInteraction();
+            si.genericPrecision_Click(VirtualKeyCode.UP);
+        }
+
+        private void leftPrecision_Click(object sender, RoutedEventArgs e)
+        {
+            SystemInteraction si = new SystemInteraction();
+            si.genericPrecision_Click(VirtualKeyCode.LEFT);
+
+        }
+
+        private void rightPrecision_Click(object sender, RoutedEventArgs e)
+        {
+            SystemInteraction si = new SystemInteraction();
+            si.genericPrecision_Click(VirtualKeyCode.RIGHT);
+        }
+
+        private void downPrecision_Click(object sender, RoutedEventArgs e)
+        {
+            SystemInteraction si = new SystemInteraction();
+            si.genericPrecision_Click(VirtualKeyCode.DOWN);
+        }
+
+
+
+        private void hideApp_StylusEnter(object sender, System.Windows.Input.StylusEventArgs e)
+        {
+            MainContainer.Opacity = 1;
+        }
+
+        private void hideApp_StylusLeave(object sender, System.Windows.Input.StylusEventArgs e)
+        {
+            MainContainer.Opacity = 0.3;
+        }
     }
 }
--- ClipTheSurface/SystemInteraction.cs
+++ ClipTheSurface/SystemInteraction.cs
@@ -1,15 +1,24 @@
 ﻿using GregsStack.InputSimulatorStandard;
 using GregsStack.InputSimulatorStandard.Native;
+using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
+
+
 namespace ClipTheSurface
 {
+
     class SystemInteraction
     {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        public bool OriginalTBHidden;
         public void genericToogle_Click(VirtualKeyCode key, Button keyButton)
         {
             var simulator = new InputSimulator();
@@ -18,12 +27,25 @@ namespace ClipTheSurface
             if (isKeyDown)
             {
                 simulator.Keyboard.KeyUp(key);
-                keyButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#7F333333");
+                keyButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF333333");
             }
             else
             {
                 simulator.Keyboard.KeyDown(key);
-                keyButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#BF536280");
+                keyButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF536280");
+            }
+        }
+
+        public void genericPrecision_Click(VirtualKeyCode key)
+        {
+            Process p = Process.GetProcessesByName("CLIPStudioPaint").FirstOrDefault();
+            var simulator = new InputSimulator();
+
+            if (p != null)
+            {
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+                simulator.Keyboard.KeyPress(key);
             }
         }
 
@@ -47,11 +69,74 @@ namespace ClipTheSurface
         {
             //This function cleans any modifier key status.
             var simulator = new InputSimulator();
-            VirtualKeyCode[] keys = { VirtualKeyCode.SHIFT, VirtualKeyCode.LCONTROL, VirtualKeyCode.LMENU };
+            VirtualKeyCode[] keys = { VirtualKeyCode.LSHIFT, VirtualKeyCode.LCONTROL, VirtualKeyCode.LMENU };
             foreach (VirtualKeyCode vky in keys)
             {
                 simulator.Keyboard.KeyUp(vky);
             }
+            OriginalTBHidden = IsTaskBarAutoHidden();
         }
+
+        private bool IsTaskBarAutoHidden()
+        {
+            return Math.Abs(SystemParameters.PrimaryScreenHeight - SystemParameters.WorkArea.Height) > 0;
+        }
+
+        public Boolean fullFocusMode(Boolean ffmode, Button keyButton)
+        {
+            Boolean changeModeTo = false;
+            Process p = Process.GetProcessesByName("CLIPStudioPaint").FirstOrDefault();
+            var simulator = new InputSimulator();
+
+            if (p != null)
+            {
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+
+                if (!ffmode)
+                {
+                    simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LSHIFT, VirtualKeyCode.TAB);
+                    simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LSHIFT, VirtualKeyCode.TAB);
+
+                    if (!OriginalTBHidden) { 
+                    ProcessCallVoid("powershell.exe", "-command \"&{$p='HKCU:SOFTWARE\\Microsoft\\Windows\\" +
+                        "CurrentVersion\\Explorer\\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;" +
+                        "&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}\"");
+                    }
+
+
+                    keyButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF536280");
+                    changeModeTo = true;
+                }
+                else
+                {
+                    simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LSHIFT, VirtualKeyCode.TAB);
+
+                    if (!OriginalTBHidden) {
+                        ProcessCallVoid("powershell.exe", "-command \"&{$p='HKCU:SOFTWARE\\Microsoft\\Windows\\" +
+                        "CurrentVersion\\Explorer\\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=2;" +
+                        "&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}\"");
+                    }
+
+                    keyButton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF333333");
+                    changeModeTo = false;
+                }
+            }
+            return changeModeTo;
+        }
+
+        public void ProcessCallVoid(string FileName, string Arguments)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = FileName;
+                process.StartInfo.Arguments = Arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = false;
+                process.Start();
+            }
+        }
+
     }
 }
